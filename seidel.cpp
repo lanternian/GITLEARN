24f0c38a4b897de7917df3e28d361e3ebc3445c9
lantern <2050216062@qq.com>
2025-03-16 18:55:27 +0800

    缓存优化

--- seidel.cpp
+++ seidel.cpp
@@ -11,6 +11,8 @@ void* my_malloc(uint64_t size)
 
 void my_seidel(int n, int T, double *A)
 {
+    double inv = 1.0 / 9.0;
+
     double* B = (double*)malloc(n * n * sizeof(double));
     memcpy(B, A, n * n * sizeof(double));
 
@@ -32,12 +34,16 @@ void my_seidel(int n, int T, double *A)
                     double* dst = (t % 2 == 0) ? B : A;
 
                     for (int i = start; i < end; i++) {
+                        double uprow = dst[(i - 1) * n] + dst[(i - 1) * n + 1] + dst[(i - 1) * n + 2];
+                        double downrow = src[(i + 1) * n] + src[(i + 1) * n + 1] + src[(i + 1) * n + 2];
                         for (int j = 1; j < n - 1; j++) {
                             dst[i * n + j]
-                            = (dst[(i - 1) * n + j - 1] + dst[(i - 1) * n + j] + dst[(i - 1) * n + j + 1]
-                            + dst[      i * n + j - 1] + src[      i * n + j] + src[      i * n + j + 1]
-                            + src[(i + 1) * n + j - 1] + src[(i + 1) * n + j] + src[(i + 1) * n + j + 1])
-                            / 9.0;
+                                = (uprow
+                                + dst[      i * n + j - 1] + src[      i * n + j] + src[      i * n + j + 1]
+                                + downrow)
+                                * inv;
+                            uprow += (dst[(i - 1) * n + j + 2]-dst[(i - 1) * n + j - 1]);
+                            downrow += (src[(i + 1) * n + j + 2]-src[(i + 1) * n + j - 1]);
                         }
                     }
                 }
