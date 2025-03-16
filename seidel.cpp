e16125141652cb4f2bbef8c7fca606ff64a43161
lantern <2050216062@qq.com>
2025-03-16 13:57:57 +0800

    双数组交替减少读写竞争

--- seidel.cpp
+++ seidel.cpp
@@ -10,12 +10,21 @@ void* my_malloc(uint64_t size)
 
 void my_seidel(int n, int T, double *A)
 {
-    for (int t = 0; t < T; t++)
+    double* B = (double*)malloc(n * n * sizeof(double));
+    memcpy(B, A, n * n * sizeof(double));
+    for (int t = 0; t < T; t++){
+        double* src = (t % 2 == 0) ? A : B;
+        double* dst = (t % 2 == 0) ? B : A;
         for (int i = 1; i < n - 1; i++)
             for (int j = 1; j < n - 1; j++)
-                    A[i * n + j]
-                = (A[(i - 1) * n + j - 1] + A[(i - 1) * n + j] + A[(i - 1) * n + j + 1]
-                         + A[      i * n + j - 1] + A[      i * n + j] + A[      i * n + j + 1]
-                         + A[(i + 1) * n + j - 1] + A[(i + 1) * n + j] + A[(i + 1) * n + j + 1])
+	            dst[i * n + j]
+                = (dst[(i - 1) * n + j - 1] + dst[(i - 1) * n + j] + dst[(i - 1) * n + j + 1]
+		         + dst[      i * n + j - 1] + src[      i * n + j] + src[      i * n + j + 1]
+		         + src[(i + 1) * n + j - 1] + src[(i + 1) * n + j] + src[(i + 1) * n + j + 1])
                  / 9.0;
+    }
+    // 确保最终结果在A中
+    if(T % 2 == 1) {
+        memcpy(A, B, n * n * sizeof(double));
+    }
 }
